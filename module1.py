4ee334b051578d0804722edc1ac3a45429018c9a
cedec1985 <69531112+cedec1985@users.noreply.github.com>
2020-08-26 15:00:33 +0200

    ajout d'un autre fichier

---
+++ module1.py
@@ -0,0 +1,6 @@
+x=(1)
+sometest=["hello","world",":)"]                                   # répéter à l'infini le mot "world"
+while x<3: 
+      print (sometest[x])                                        # on casse la répétition avec break        
+x +=1
+
